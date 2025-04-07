a24d3a3b1c103b9c6066f44b0503e91e063c6dab
piolonrqz <piolofrances.enriquez@cit.edu>
2025-04-07 08:55:28 +0800

    implemented admin page design

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -4,6 +4,8 @@ import Homepage from './pages/Homepage.jsx';
 import SpacesPage from './pages/SpacesPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
+import AdminPage from './pages/AdminPage.jsx';
+import ProtectedAdminRoute from './Components/ProtectedAdminRoute.jsx';
 // ... other page imports
 
 function App() {
@@ -11,10 +13,18 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        {/* Make sure you have this route defined */}
         <Route path="/SpacesPage" element={<SpacesPage />} />
         <Route path="/LoginPage" element={<LoginPage />} />
         <Route path="/RegisterPage" element={<RegisterPage />} />
+        {/* Protect the AdminPage route */}
+        <Route
+          path="/AdminPage"
+          element={
+            <ProtectedAdminRoute>
+              <AdminPage />
+            </ProtectedAdminRoute>
+          }
+        />
         {/* ... other routes ... */}
       </Routes>
     </Router>
